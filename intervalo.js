f832727192561673e5bb852d6fa0b690ecbba2d4
guilherme.andreta <guilherme.andreta@santacasa.sjrp>
2023-06-14 15:46:45 -0300

    Adicionado Intervalo.js e feito o EX 1037

--- intervalo.js
+++ intervalo.js
@@ -2,7 +2,7 @@ import fs from "fs";
 var input = fs.readFileSync("stdin", "utf-8");
 
 // Fazendo com que o valor seja separado ao dar enter
-var valores = input.split('\n');
+var valores = input.split("\n");
 
 // Recebendo valor e colocando como Float
 var entrada = parseFloat(valores.shift());
@@ -10,25 +10,16 @@ var entrada = parseFloat(valores.shift());
 // Variaveis para geração de intervalos
 const intervalo = [0, 25, 50, 75, 100];
 
-for(let i = 0; i < intervalo.length; i++) {
-
+for (let i = 0; i < intervalo.length; i++) {
     if (entrada > 100) {
-
         console.log("Fora de intervalo");
         break;
-
-    }else if(entrada > intervalo[i]) {
-
+    } else if (entrada > intervalo[i]) {
         var inicio = intervalo[i];
-
-    }else if(entrada <= intervalo[i]) {
-
+    } else if (entrada <= intervalo[i]) {
         var final = intervalo[i];
 
         console.log("Intervalo [%d,%d]", inicio, final);
         break;
-
     }
-
 }
-
